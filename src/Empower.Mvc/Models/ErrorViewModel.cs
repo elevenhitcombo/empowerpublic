efae3ba0bf7bb9907e29ce6059b0c27fadb19dd2
Paul Taylor <pap@elevenhitcombo.com>
2018-07-09 14:56:35 +0100

    First clean - cleaning up the rubbish

---
+++ src/Empower.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Empower.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
