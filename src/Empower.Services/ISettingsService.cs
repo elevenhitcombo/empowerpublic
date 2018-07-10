77aac2bd6468dcd9562254add6458250f6108619
Paul Taylor <pap@elevenhitcombo.com>
2018-07-10 14:46:25 +0100

    Baseline build

---
+++ src/Empower.Services/ISettingsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Empower.Services
+{
+    public interface ISettingsService
+    {
+        string GetStringValue(string key);
+        int GetIntValue(string key);
+        object GetValue(string key);
+    }
+}
